e09d1e60b4e6314188f0913ab0c0ec42c5233bb0
Stien Vanhoutte <STIEN@VANHOUTTE.EU.COM>
2022-08-11 12:11:33 +0000

    function generate random number

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def generate_random_number(): 
+    var= random.randint(1, 99)
+    print(var)
+
+
+def main():
+    generate_random_number()
+
+main() 
