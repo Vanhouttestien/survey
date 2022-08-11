d392ead63954319df92e6cb294e66d9c155f1742
Stien Vanhoutte <STIEN@VANHOUTTE.EU.COM>
2022-08-11 16:44:57 +0000

    add loop to add a number every time you play

--- run.py
+++ run.py
@@ -2,46 +2,53 @@ import random
 import time
 import os
 
+level=1
+
 def generate_random_number(): 
     """
     Print a random number and make it disappear after 20 seconds
     """
-    var= random.randint(1, 99)
-    print(var)
-    time.sleep(20)
+    global level
+    ls=[]
+    for i in range(level): 
+        var = random.randint(1, 99)
+        ls.append(var)
+    print(ls)
+    level +=1
+    print(level)
+    return level
+    time.sleep(2)
     os.system('clear')
 
-def user_input():
-    """
-    User can fill the numbers in.
-    """
-    while True:
-        try: 
-            print('enter the numbers seperated with a comma')
-            print('32, 5, 99, 43')
-            x = input('Enter the numbers:')
-            guessed_numbers= (x.strip()).split(",")
-            guessed_numbers_int = [eval(i) for i in guessed_numbers]  # convert list numbers to list of strings https://www.geeksforgeeks.org/python-converting-all-strings-in-list-to-integers/
-        except NameError:
-            print("not a number")
+# def user_input():
+#     """
+#     User can fill the numbers in.
+#     """
+#     while True:
+#         try: 
+#             print('enter the numbers seperated with a comma')
+#             print('32, 5, 99, 43')
+#             x = input('Enter the numbers:')
+#             guessed_numbers= (x.strip()).split(",")
+#             guessed_numbers_int = [eval(i) for i in guessed_numbers]  # convert list numbers to list of strings https://www.geeksforgeeks.org/python-converting-all-strings-in-list-to-integers/
+#         except NameError:
+#             print("not a number")
         
-    return guessed_numbers_int
+#     return guessed_numbers_int
 
-        # if validate(guessed_numbers_int):
-        #     print("this is valid")
-        #     break
-        
-"""
-def validate(values):
-    try:
-        [int(value) for value in values]
-    except ValueError:
-        print('Not a number')
-"""        
-    
+def check_correct():
+    global level
+    if level<10:
+         main()
+    else: 
+        print("stop")
+
+def correct_add_number():
+    generate_random_number()
 
 def main():
-    #generate_random_number()
-    user_input()
+    generate_random_number()
+    #user_input()
+    check_correct()
 
 main()
