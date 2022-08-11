42ac06ef7e97630cabd772bb1d6933cca4ec2aa4
Stien Vanhoutte <STIEN@VANHOUTTE.EU.COM>
2022-08-11 12:21:22 +0000

    make number dissappear after 20 seconds

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import random
+import time
+import os
 
 def generate_random_number(): 
     var= random.randint(1, 99)
     print(var)
+    time.sleep(20)
+    os.system("CLS")
 
 
 def main():
     generate_random_number()
 
-main() 
+main()
