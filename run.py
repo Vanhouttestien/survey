dda771f3b06adc97889fb38d9c6ac80280a3f795
Stien Vanhoutte <STIEN@VANHOUTTE.EU.COM>
2022-08-12 12:14:20 +0000

    connect google sheets and created highscore list

--- run.py
+++ run.py
@@ -2,27 +2,57 @@ import random
 import time
 import os
 from getkey import getkey, keys
+import gspread
+from google.oauth2.service_account import Credentials
 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('nummemory').worksheet('score')
+
+
+
+
+def score_update():
+    global nickname
+    global level
+    score= SHEET.insert_row([nickname,level], index=2)
+    SHEET.sort((2,'asc'))
+    highscore_list=SHEET.get_values('A1:B11')
+    print(highscore_list)
+    
+
+#global variables
 level=1
 ls=[]
 input_number=[]
+nickname= ""
+
+
 def instructions():
     print("Can you remember all the numbers?")
     print("The goal of the game is to try to remember as much numbers as you can.")
     print("The order of the numbers doesn't mather.")
-    print('enter the numbers seperated with a comma')
-    print(' for example: 32, 5, 99, 43')
+    print('enter the numbers seperated with whitespace')
+    print('For example: 32 5 99 43')
     print("try to repeat the numbers you see and level up.")
     print("How far can you get?")
     print()
     print('Enter a nickname:')
+    global nickname
     nickname = input()
     print()
     print("Press Enter to get started")
     key = getkey()
     if key == keys.ENTER:
         print("upressed enter")
-
+    return nickname
 
 
 def generate_random_number(): 
@@ -34,8 +64,9 @@ def generate_random_number():
     for i in range(level): 
         var = random.randint(1, 99)
         ls.append(var)
-    print(ls)
     print(f'level: {level}')
+    print(ls)
+    print("Wait for 20 seconds or press ENTER")
     key = getkey()
     if  key == keys.ENTER:
         os.system('clear')
@@ -51,18 +82,18 @@ def user_input():
     global input_number
     while True:
         try: 
-            print('enter the numbers seperated with a comma')
-            print(' ex. 32, 5, 99, 43')
+            print('enter the numbers seperated with a whitespace')
+            print(' ex. 32 5 99 43')
             x = input('Enter the numbers:')
-            guessed_numbers= (x.strip()).split(",")
+            guessed_numbers = (x.strip()).split(" ")
             guessed_numbers_int = [eval(i) for i in guessed_numbers]  # convert list numbers to list of strings https://www.geeksforgeeks.org/python-converting-all-strings-in-list-to-integers/
-            input_number=guessed_numbers_int
+            input_number = guessed_numbers_int
             return input_number
         except NameError:
             print("not a number")
         except SyntaxError:
             print("did you use the right format?")
-            print(' ex. 32, 5, 99, 43')
+            print(' ex. 32 5 99 43')
         
     return guessed_numbers_int, ls
 
@@ -74,14 +105,18 @@ def check_correct():
     input_number.sort()
     if input_number == ls: 
         ls=[]
-        level +=1
+        level += 1
+        os.system('clear')
         main()
     else: 
         end_game()
         
 
 def end_game():
-    print("stop")
+    print("Wrong: you lost")
+    print(f'the right answer was {ls}')
+    print(f'You got till level {level}')
+    score_update()
 
 def main():
     generate_random_number()
