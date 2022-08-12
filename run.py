7fec4be4e288734b83ce45f4c8c6d31bd6ea2d5d
Stien Vanhoutte <STIEN@VANHOUTTE.EU.COM>
2022-08-12 22:00:51 +0000

    install colorama libery and added color to text

--- run.py
+++ run.py
@@ -5,6 +5,9 @@ from getkey import getkey, keys
 import gspread
 from google.oauth2.service_account import Credentials
 from tabulate import tabulate
+from colorama import init
+from colorama import Fore, Back, Style
+from pyfiglet import Figlet
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -30,27 +33,30 @@ nickname= ""
 
 
 def instructions():
+    custom_fig = Figlet(font='computer')    # code from https://www.devdungeon.com/content/create-ascii-art-text-banners-python
+    print(custom_fig.renderText('Nummory'))
     print("Can you remember all the numbers?")
     print("The goal of the game is to try to remember as much numbers as you can.")
     print("The order of the numbers doesn't mather.")
-    print('enter the numbers seperated with whitespace')
-    print('For example: 32 5 99 43')
-    print("try to repeat the numbers you see and level up.")
+    print('Enter the numbers seperated with whitespace')
+    print(Fore.GREEN + 'For example: 32 5 99 43')
+    print(Fore.WHITE + "Try to repeat all the numbers you see and level up.")
     print("How far can you get?")
     print()
     print('Enter a nickname and press ENTER')
-    print('nickname:',end="")
+    print(Fore.BLUE+'nickname:',end="")
     global nickname
     while True: 
         try:    
             nickname = input()
             1/len(nickname)
-            print(f'Welcome {nickname}')
+            print(Fore.GREEN + f'Welcome {nickname}')
             time.sleep(2)
+
             os.system('clear')
             return nickname
         except ZeroDivisionError:
-            print('Please enter a nickname.')
+            print(Fore.RED + 'Please enter a nickname.')
     
 def generate_random_number(): 
     """
@@ -61,10 +67,11 @@ def generate_random_number():
     for i in range(level): 
         var = random.randint(1, 99)
         ls.append(var)
+    print(Style.RESET_ALL)
     print("Wait for 20 seconds or press any key")
     print(f'level: {level}')
     print()
-    print('Numbers:')
+    print(Fore.BLUE + 'Numbers:')
     print(*ls, sep = ',')
     key = getkey()
     if  key == key:
@@ -81,22 +88,23 @@ def user_input():
     global input_number
     while True:
         try: 
+            print(Style.RESET_ALL)
             print('Have you remembered them all?')
             print()
             print()
-            print('Enter the numbers:',end="")
+            print(Fore.BLUE+'Enter the numbers:',end="")
             x = input()
             guessed_numbers = (x.strip()).split(" ")
             guessed_numbers_int = [eval(i) for i in guessed_numbers]  # convert list numbers to list of strings https://www.geeksforgeeks.org/python-converting-all-strings-in-list-to-integers/
             input_number = guessed_numbers_int
             return input_number
         except NameError:
-            print("not a number")
-            print(' sperate the numbers by a whitespace: ex. 32 5 99 43')
+            print(Fore.RED + "You didn't type a number")
+            print(Fore.RED +'seperate the numbers by a whitespace: ex. 32 5 99 43')
         except SyntaxError:
-            print("did you use the right format?")
-            print("you can only use numbers and they should be seperated by a whitespace")
-            print(' ex. 32 5 99 43')
+            print(Fore.RED + "did you use the right format?")
+            print(Fore.RED + "you can only use numbers and they should be seperated by a whitespace")
+            print(Fore.RED +' ex. 32 5 99 43')
         
     return guessed_numbers_int, ls
 
@@ -121,7 +129,7 @@ def check_correct():
         
 
 def end_game():
-    print("You gave the wrong answer")
+    print(Fore.RED + "You gave the wrong answer")
     print(f'the right answer was {ls}')
     print(f'You got till level {level}')
     score_update()
@@ -136,7 +144,7 @@ def score_update():
     SHEET.sort((2,'des'))
     highscore_list=SHEET.get_values('A2:B11')       #code for making a table: https://www.statology.org/create-table-in-python/
     col_names = ["NAME", "LEVEL"]  
-    print(tabulate(highscore_list, headers=col_names))
+    print(Fore.LIGHTMAGENTA_EX + tabulate(highscore_list, headers=col_names))
 
 def main():
     generate_random_number()
